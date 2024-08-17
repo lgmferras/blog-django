4d6b0f9a62c9d2e668278f416c6f9de4fe4b778b
lgmferras <lgmferras@gmail.com>
2024-08-17 15:10:27 -0300

    Subida 17/08/24 - 15h00

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,8 +1,34 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+posts = list(range(1000))
 
 # Create your views here.
 def index(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
         'title': 'Home',
+        'page_obj': page_obj,
+
     }
     return render(request, 'blog/pages/index.html', context)
+
+def page(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': 'Page',
+    }
+    return render(request, 'blog/pages/page.html', context)
+
+def post(request):
+    paginator = Paginator(posts, 9)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'title': 'Post',
+    }
+    return render(request, 'blog/pages/post.html', context)
--- djangoapp/site_setup/admin.py
+++ djangoapp/site_setup/admin.py
@@ -3,17 +3,19 @@ from django.http import HttpRequest
 from site_setup.models import MenuLink, SiteSetup
 
 
-# Register your models here.
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
+# @admin.register(MenuLink)
+# class MenuLinkAdmin(admin.ModelAdmin):
+#     class Meta:
+#         verbose_name = 'Menu Link'
+#         verbose_name_plural = 'Menu Links'
 
-    list_display = ('id', 'text', 'url_or_path',)
-    list_display_links = ('id', 'text', 'url_or_path',)
-    search_fields = ('id', 'text', 'url_or_path',)
+#     list_display = ('id', 'text', 'url_or_path',)
+#     list_display_links = ('id', 'text', 'url_or_path',)
+#     search_fields = ('id', 'text', 'url_or_path',)
 
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
 
 @admin.register(SiteSetup)
 class SiteSetupAdmin(admin.ModelAdmin):
@@ -22,6 +24,7 @@ class SiteSetupAdmin(admin.ModelAdmin):
         verbose_name_plural = 'Setup'
 
     list_display = ('title', 'description',)
+    inlines = (MenuLinkInline,)
 
     def has_add_permission(self, request):
         return not SiteSetup.objects.exists()
